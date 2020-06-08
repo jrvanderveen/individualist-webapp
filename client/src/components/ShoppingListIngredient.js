3cd72637fdcd15eec605e4cc7f713336d574e026
Johan van der Veen <wasjrvanderveen@gmail.com>
2020-06-08 16:21:16 -0700

    added shopping list ingredient component

---
+++ client/src/components/ShoppingListIngredient.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { List, Wrapper } from "../elements/index";
+
+const Span = styled.span`
+    ${(props) =>
+        props.lineThrough === true &&
+        `
+        color: red;
+        text-decoration: line-through;
+    `};
+`;
+
+export const ShoppingListIngredient = (props) => {
+    const [active, setActive] = useState(false);
+
+    return (
+        <List key={props.ingredient} isShoppingList>
+            <Span lineThrough={active}>
+                <Wrapper>{props.ingredient}</Wrapper>
+            </Span>
+            <button className="float-right btn btn-danger btn-sm" onClick={() => setActive(!active)}>
+                X
+            </button>
+        </List>
+    );
+};
