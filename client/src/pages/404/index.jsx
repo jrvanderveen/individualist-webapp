baec9cf51c90fd349474905305d3b62900cf1a34
Johan van der Veen <wasjrvanderveen@gmail.com>
2020-08-21 10:08:59 -0700

    Added link to back to home page

--- client/src/pages/404/index.jsx
+++ client/src/pages/404/index.jsx
@@ -2,11 +2,28 @@ import React from "react";
 import styled from "styled-components";
 
 // Styled Components
+const Button = styled.button`
+    cursor: pointer;
+    background-color: ${(props) => (props.clicked === true ? "#8171d1" : "#9c88ff")};
+    box-shadow: var(--box-shadow);
+    color: #fff;
+    border: 0;
+    display: flex;
+    align-items: center;
+    padding: 5px 60px;
+    font-size: 25px;
+
+    &:hover {
+        background-color: #8171d1;
+    }
+`;
+
 const Div = styled.div`
     height: 90vh;
     display: flex;
     align-items: center;
     justify-content: center;
+    flex-direction: column;
 `;
 /*
     SUMMARY:
@@ -15,10 +32,13 @@ const Div = styled.div`
     PARAMS: 
 
 */
-export const NotFound = () => {
+export const NotFound = ({ history }) => {
     return (
         <Div>
             <h2>404! Not Found.</h2>
+            <Button seperate onClick={() => history.push("/")}>
+                Home
+            </Button>
         </Div>
     );
 };
