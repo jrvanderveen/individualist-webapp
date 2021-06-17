8dac9125344db1282e12b64bf6afa0ff673fe373
Johan van der Veen <wasjrvanderveen@gmail.com>
2021-06-17 11:27:40 -0700

    Add pages

---
+++ client/src/components/common/mealType/index.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import DropdownButtonImported from "react-bootstrap/DropdownButton";
+import DropdownImported from "react-bootstrap/Dropdown";
+import './style.css'
+
+/*
+    SUMMARY:
+        Drop down button for meal types
+
+    PARAMS: 
+
+*/
+
+export const MealTypesDropDown = ({ defaultType, types, onChange, size }) => {
+
+    // Context
+    const [mealType, setMealType] = useState(defaultType);
+    
+    //functions
+    useEffect(() => {
+        if(mealType === ""){
+            handleChange(defaultType)
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [defaultType])
+
+    const handleChange = (text) => {
+        onChange(text)
+        setMealType(text)
+    }
+
+    return (
+        <DropdownButtonImported className="dropdown-button-drop-right" drop="right" size={size} variant="info" title={mealType}>
+            {types.map((type, index) => (
+                <DropdownImported.Item key={index} onClick={(e) => handleChange(e.target.text)} eventKey={index + 1}>
+                    {type}
+                </DropdownImported.Item>
+            ))}
+        </DropdownButtonImported>
+    );
+};
